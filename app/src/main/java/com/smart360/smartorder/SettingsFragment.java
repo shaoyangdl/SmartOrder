7dbcb1263e33bbe49ff67618bb747d70a189d291
Yang <shaoyang@me.com>
2015-08-24 20:50:13 +1000

    1st version

---
+++ app/src/main/java/com/smart360/smartorder/SettingsFragment.java
@@ -0,0 +1,28 @@
+package com.smart360.smartorder;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment{
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
